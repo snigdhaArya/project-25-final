015138c607f8cd5726d4decfd2aa7b82742d36ed
snigdhaArya <76739977+snigdhaArya@users.noreply.github.com>
2021-03-27 21:23:54 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,34 @@
+class paper {
+constructor(x,y,r)
+{
+    var option={
+        isStatic:false,
+        restitution:0.5,
+        friction:0,
+        density:1.2
+    }
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.image=loadImage("paper.png");
+    this.body=Bodies.circle(this.x,this.y,this.r,option)
+    World.add(world,this.body);
+
+}
+display()
+{
+    var paperpos=this.body.position;
+    push()
+    translate(paperpos.x,paperpos.y);
+    rectMode(CENTER)
+    fill(255,0,255);
+    imageMode(CENTER)
+    // circleMode(CENTER)
+    // circle(this.x,this.y,this.r);
+    image(this.image,0,0,this.r,this.r)
+    pop()
+}
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var paperobject;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+    paperobject=new paper(200,630,60);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+//  keypress();
+  groundObject.display();
+  dustbinObj.display();
+  paperobject.display();
+
+}
+
+function keyPressed() {
+
+if(keyCode===UP_ARROW){
+
+	Matter.Body.applyForce(paperobject.body,paperobject.body.position,{x:230,y:-645});
+
+}
+
+}
